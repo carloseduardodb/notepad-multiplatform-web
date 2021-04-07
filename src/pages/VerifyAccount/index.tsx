ad6c833432fa2b0ad868c2fc193c062c77f1a565
carloseduardodb <carloscangere@hotmail.com>
2021-04-07 12:15:36 -0300

    add page and organize verify account

---
+++ src/pages/VerifyAccount/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Content } from './styles';
+import InputText from '../../components/InputText';
+import Button from '../../components/Button';
+
+const VerifyAccount = () => {
+  return (
+    <Content>
+      <form className="form-body"></form>
+    </Content>
+  );
+};
+
+export default VerifyAccount;
---
+++ src/pages/VerifyAccount/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+export const Content = styled.div`
+  height: 100vh;
+  background-color: #fff;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: rgb(191, 155, 111);
+  background: radial-gradient(
+    circle,
+    rgba(191, 155, 111, 1) 0%,
+    rgba(27, 27, 27, 1) 30%
+  );
+  h3 {
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+
+  .form-body {
+    background-color: #353440;
+    padding: 30px;
+    border-radius: 10px;
+    width: 60%;
+    max-width: 500px;
+    position: relative;
+    .return-login {
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin: 40px;
+    }
+  }
+
+  div.options {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    a {
+      color: #6f5f54;
+      font-size: 11pt;
+    }
+  }
+`;
