2e162ff951a4bfe6d9ae248cbde0821b0ae9cc2e
carloseduardodb <carloscangere@hotmail.com>
2021-04-08 16:47:43 -0300

    add unform and new Form tag

--- src/pages/ResendEmail/index.tsx
+++ src/pages/ResendEmail/index.tsx
@@ -5,11 +5,12 @@ import logo from '../../assets/Logo.svg';
 import Button from '../../components/Button';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 const ResendEmail = () => {
   return (
     <Content>
-      <form className="form-body">
+      <Form onSubmit={() => {}} className="form-body">
         <img src={logo} width="40%" alt="Notepad Multiplatform" />
         <Link className="return-login" to="/login">
           <FiArrowLeft color="#fff" size={40} />
@@ -17,10 +18,10 @@ const ResendEmail = () => {
         <h3>Reenviar email:</h3>
         <InputText type="email" name="email" placeholder="Email" />
         <div style={{ marginTop: -10 }}>
-          <Button text="Reenviar" color="#F2808A" />
-          <Button text="Fazer Login" color="#BF9B6F" />
+          <Button type="button" text="Reenviar" color="#F2808A" />
+          <Button type="button" text="Fazer Login" color="#BF9B6F" />
         </div>
-      </form>
+      </Form>
     </Content>
   );
 };
