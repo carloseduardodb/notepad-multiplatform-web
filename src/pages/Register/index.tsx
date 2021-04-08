22b1b5bdd9965211df4d6b05b9681cda087de47a
carloseduardodb <carloscangere@hotmail.com>
2021-04-08 16:52:16 -0300

    Modified the page by adding support for unform

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -4,11 +4,12 @@ import logo from '../../assets/Logo.svg';
 import Button from '../../components/Button';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 const Register = () => {
   return (
     <Content>
-      <form className="form-body">
+      <Form onSubmit={() => {}} className="form-body">
         <img src={logo} width="40%" alt="Notepad Multiplatform" />
         <Link className="return-login" to="/login">
           <FiArrowLeft color="#fff" size={40} />
@@ -33,7 +34,7 @@ const Register = () => {
             <Button text="Registrar" color="#F2808A" />
           </Link>
         </div>
-      </form>
+      </Form>
     </Content>
   );
 };
