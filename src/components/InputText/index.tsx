7960c71381d46690bf1891f52dea4c5c55da6199
carloseduardodb <carloscangere@hotmail.com>
2021-04-08 16:50:36 -0300

    changed input to endure lib unform

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { Input as IPT, Content } from './styles';
+import { useField } from '@unform/core';
 
-interface Props {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   placeholder: string;
   type: string;
 }
 
 const Input: React.FC<Props> = ({ name, placeholder, type, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef,
+      getValue: (ref) => {
+        return ref.current.value;
+      },
+      setValue: (ref, value) => {
+        ref.current.value = value;
+      },
+      clearValue: (ref) => {
+        ref.current.value = '';
+      },
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Content>
-      <IPT type={type} id={name} placeholder={placeholder} />
+      <IPT
+        ref={inputRef}
+        defaultValue={defaultValue}
+        type={type}
+        id={name}
+        placeholder={placeholder}
+        {...rest}
+      />
     </Content>
   );
 };
