34f6e0554efddb8c95f8fe47ef64e03d9c837dae
carloseduardodb <carloscangere@hotmail.com>
2021-04-10 16:06:35 -0300

    add lock pages if not executing login

--- src/utils/monitorLocation.ts
+++ src/utils/monitorLocation.ts
@@ -6,16 +6,21 @@ const monitorLocation = () => {
   const path = useLocation();
   const name_router = path.pathname;
 
+  const token = localStorage.getItem('user_token');
+  const email = localStorage.getItem('user_email');
+
   if (
     name_router == '/login' ||
     name_router == '/register' ||
     name_router == '/verify-account' ||
     name_router == '/resend-email'
   ) {
-    const token = localStorage.getItem('user_token');
-    const email = localStorage.getItem('user_email');
     if (token != null && email != null) {
       history.push('home');
+    }
+  } else {
+    if (token == null && email == null) {
+      history.push('login');
     }
   }
 };
