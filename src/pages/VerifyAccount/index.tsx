469cb7615d6d07e12880d0ff573d738fd1527764
carloseduardodb <carloscangere@hotmail.com>
2021-04-10 14:14:37 -0300

    Update code verification input

--- src/pages/VerifyAccount/index.tsx
+++ src/pages/VerifyAccount/index.tsx
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Content } from './styles';
 import InputText from '../../components/InputText';
 import Button from '../../components/Button';
 import logo from '../../assets/Logo.svg';
 import ReactCodeInput from 'react-verification-code-input';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 import { FiArrowLeft } from 'react-icons/fi';
+import { toast, ToastContainer } from 'react-toastify';
 
 const VerifyAccount = () => {
+  const [code, setCode] = useState('');
+  const history = useHistory();
+
+  const config = {
+    Accept: 'application/json',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('user_token')}`,
+      'Content-Type': 'application/json',
+    },
+  };
+
+  function handleSubmit() {
+    if (code.length === 7) {
+      sendCode();
+    } else {
+      toast.error('Ooooops código muito pequeno!');
+    }
+  }
+
+  function sendCode() {
+    const hidrated_data = {
+      email: localStorage.getItem('user_email'),
+      code: code,
+    };
+    api
+      .post('user/code-validation', hidrated_data, config)
+      .then((response) => {
+        if (response.status === 201) {
+          history.push('/home');
+        } else {
+          toast.error(
+            'Ooooops algo de errado não esta certo! Seu código está incorreto!',
+          );
+        }
+      })
+      .catch((error) => {
+        toast.error(
+          'Ooooops algo de errado não esta certo! Seu código está incorreto!',
+        );
+      });
+  }
+
   return (
     <Content>
       <form className="form-body">
@@ -16,9 +60,15 @@ const VerifyAccount = () => {
           <FiArrowLeft color="#fff" size={40} />
         </Link>
         <h3>Verificação de email</h3>
-        <ReactCodeInput fields={7} />
-        <Button color="#BF9B6F" text="Verificar" />
+        <ReactCodeInput type="text" onChange={setCode} fields={7} />
+        <Button
+          type="button"
+          onClick={handleSubmit}
+          color="#BF9B6F"
+          text="Verificar"
+        />
       </form>
+      <ToastContainer />
     </Content>
   );
 };
