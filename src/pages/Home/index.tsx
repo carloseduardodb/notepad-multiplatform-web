610c022158e16c8330eecc2ff7180415a65789e1
carloseduardodb <carloscangere@hotmail.com>
2021-04-10 21:00:42 -0300

    update set data notes in ListNotes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Content } from './styles';
 import logo from '../../assets/Logo.svg';
 import Navbar from '../../components/Navbar';
 import ListNotes from '../../components/ListNotes';
 import monitorLocation from '../../utils/monitorLocation';
+import api from '../../services/api';
+import { toast, ToastContainer } from 'react-toastify';
 
 const dataExample = {
   note: {
@@ -14,14 +16,41 @@ const dataExample = {
   },
 };
 
+interface Note {
+  id: string;
+  title: string;
+  created_at: string;
+  text: string;
+}
+
 const Home = () => {
   monitorLocation();
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    const config = {
+      Accept: 'application/json',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('user_token')}`,
+      },
+    };
+    api
+      .get('note/show', config)
+      .then((response) => {
+        setNotes(response.data.notes.reverse());
+      })
+      .catch((error) => {
+        toast.error('Erro: ' + error.message);
+      });
+  }, []);
+
   return (
     <Content>
       <div className="dashboard">
         <Navbar />
-        <ListNotes note={dataExample.note} />
+        <ListNotes notes={notes} />
       </div>
+      <ToastContainer />
     </Content>
   );
 };
